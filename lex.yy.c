971d114c93bad1679cc2fa027eba50afacdd1fca
hunterAntal <hunterantalgithub@gmail.com>
2024-09-23 15:26:36 -0400

    project1 is now working. Not finished

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 10
-#define YY_END_OF_BUFFER 11
+#define YY_NUM_RULES 11
+#define YY_END_OF_BUFFER 12
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,11 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[22] =
+static const flex_int16_t yy_accept[19] =
     {   0,
-        0,    0,   11,    8,    9,    8,    1,    2,    3,    4,
-        6,    5,    0,    7,    1,    2,    3,    4,    6,    5,
-        0
+        0,    0,   12,   11,   10,   11,    1,    2,    3,    4,
+        8,    6,    5,    7,    0,    9,    8,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -374,8 +373,8 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    3,    1,    1,    1,    1,    1,    1,
         1,    4,    5,    1,    6,    1,    7,    8,    8,    8,
-        8,    8,    8,    8,    8,    8,    8,    1,    1,    1,
-        9,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        8,    8,    8,    8,    8,    8,    8,    1,    1,    9,
+       10,   11,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,39 +398,36 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[10] =
+static const YY_CHAR yy_meta[12] =
     {   0,
-        1,    2,    1,    1,    1,    1,    1,    1,    1
+        1,    2,    1,    1,    1,    1,    1,    1,    1,    1,
+        1
     } ;
 
-static const flex_int16_t yy_base[23] =
+static const flex_int16_t yy_base[20] =
     {   0,
-        0,    0,   26,   27,   27,   22,   22,   21,   20,   19,
-       18,   17,   15,   14,   14,   13,   12,   11,   10,    9,
-       27,    9
+        0,    0,   18,   19,   19,   14,   19,   19,   19,   19,
+        8,   19,   19,   19,   12,   11,    5,   19,   11
     } ;
 
-static const flex_int16_t yy_def[23] =
+static const flex_int16_t yy_def[20] =
     {   0,
-       21,    1,   21,   21,   21,   22,   21,   21,   21,   21,
-       21,   21,   22,   22,   21,   21,   21,   21,   21,   21,
-        0,   21
+       18,    1,   18,   18,   18,   19,   18,   18,   18,   18,
+       18,   18,   18,   18,   19,   19,   18,    0,   18
     } ;
 
-static const flex_int16_t yy_nxt[37] =
+static const flex_int16_t yy_nxt[31] =
     {   0,
         4,    5,    6,    7,    8,    9,   10,   11,   12,   13,
-       20,   19,   18,   17,   16,   15,   14,   14,   20,   19,
-       18,   17,   16,   15,   14,   21,    3,   21,   21,   21,
-       21,   21,   21,   21,   21,   21
+       14,   15,   17,   16,   16,   17,   16,   18,    3,   18,
+       18,   18,   18,   18,   18,   18,   18,   18,   18,   18
     } ;
 
-static const flex_int16_t yy_chk[37] =
+static const flex_int16_t yy_chk[31] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,   22,
-       20,   19,   18,   17,   16,   15,   14,   13,   12,   11,
-       10,    9,    8,    7,    6,    3,   21,   21,   21,   21,
-       21,   21,   21,   21,   21,   21
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,   19,   17,   16,   15,   11,    6,    3,   18,   18,
+       18,   18,   18,   18,   18,   18,   18,   18,   18,   18
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -450,17 +446,17 @@ int yy_flex_debug = 0;
 char *yytext;
 #line 1 "project1.l"
 #line 2 "project1.l"
-    /*Deffinitions*/
+    /* Definitions */
     #include "y.tab.h"
-    # include <stdio.h>
-    # include <stdlib.h>
-    # include <string.h>
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
 
     int yyerror(char *errormsg);
     char str[1024];
-#line 462 "lex.yy.c"
-/*Rule section*/
-#line 464 "lex.yy.c"
+#line 458 "lex.yy.c"
+/* Rule section */
+#line 460 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -677,9 +673,9 @@ YY_DECL
 		}
 
 	{
-#line 14 "project1.l"
+#line 13 "project1.l"
 
-#line 683 "lex.yy.c"
+#line 679 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -706,13 +702,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 22 )
+				if ( yy_current_state >= 19 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 27 );
+		while ( yy_base[yy_current_state] != 19 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -737,65 +733,64 @@ do_action:	/* This label is used only to access EOF actions. */
 			goto yy_find_action;
 
 case 1:
-/* rule 1 can match eol */
 YY_RULE_SETUP
-#line 15 "project1.l"
+#line 14 "project1.l"
 {printf("operator: * \n");}
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 16 "project1.l"
+#line 15 "project1.l"
 {printf("operator: + \n");}
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
 YY_RULE_SETUP
-#line 17 "project1.l"
+#line 16 "project1.l"
 {printf("operator: - \n");}
 	YY_BREAK
 case 4:
-/* rule 4 can match eol */
 YY_RULE_SETUP
-#line 18 "project1.l"
+#line 17 "project1.l"
 {printf("operator: / \n");}
 	YY_BREAK
 case 5:
-/* rule 5 can match eol */
 YY_RULE_SETUP
-#line 19 "project1.l"
+#line 18 "project1.l"
 {printf("operator: = \n");}
 	YY_BREAK
 case 6:
-/* rule 6 can match eol */
 YY_RULE_SETUP
-#line 21 "project1.l"
-{printf("Intager: %s\n", yytext);}
+#line 19 "project1.l"
+{printf("operator: < \n");}
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 23 "project1.l"
+#line 20 "project1.l"
+{printf("operator: > \n");}  /* Fix here */
+	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 21 "project1.l"
+{printf("Integer: %s\n", yytext);}  /* Fix for integers */
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 22 "project1.l"
 {strncpy(str, &(yytext[1]), strlen(yytext)-2);
 str[strlen(yytext)-2] = (char) 0;
 {printf("Quote: %s\n", str);}}
 	YY_BREAK
-case 8:
+case 10:
+/* rule 10 can match eol */
 YY_RULE_SETUP
-#line 27 "project1.l"
-{printf("unknown char\n");}
-	YY_BREAK
-case 9:
-/* rule 9 can match eol */
-YY_RULE_SETUP
-#line 31 "project1.l"
+#line 26 "project1.l"
 {return 0;}
 	YY_BREAK
-case 10:
+case 11:
 YY_RULE_SETUP
-#line 32 "project1.l"
+#line 27 "project1.l"
 ECHO;
 	YY_BREAK
-#line 799 "lex.yy.c"
+#line 794 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1092,7 +1087,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 22 )
+			if ( yy_current_state >= 19 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1120,11 +1115,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 22 )
+		if ( yy_current_state >= 19 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 21);
+	yy_is_jam = (yy_current_state == 18);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1800,7 +1795,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 32 "project1.l"
+#line 27 "project1.l"
 
 
 int yywrap(void) {
@@ -1810,12 +1805,11 @@ int yywrap(void) {
 int main(){
     printf("Enter String: \n");
     yylex();
-
     return 0;
 }
 
-int yyerror(char *errormsg)
-{
+int yyerror(char *errormsg) {
     fprintf(stderr, "%s\n", errormsg);
     exit(1);
 }
+
